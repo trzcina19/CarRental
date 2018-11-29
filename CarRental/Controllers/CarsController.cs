a71367aff3f31b42f4ee7bb298b682ba67a7b1ec
trzcina19 <trzcinski.michal.mt@gmail.com>
2018-11-29 15:37:17 +0100

    First commit

---
+++ CarRental/Controllers/CarsController.cs
@@ -0,0 +1,35 @@
+﻿using CarRental.Models;
+using CarRental.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarRental.Controllers
+{
+    public class CarsController : Controller
+    {
+        // GET: Cars
+        public ActionResult Random()
+        {
+            var car = new Car() { Name = "Skoda Octavia" };
+            var customers = new List<Customer>
+            {
+                new Customer {Name="Customer 1"},
+                new Customer {Name="Customer 2"}
+            };
+
+            var viewModel = new RandomCarViewModel()
+            {
+                Car = car,
+                Customers = customers
+            };
+
+
+            return View(viewModel);
+        }
+
+     
+    }
+}
---
+++ CarRental/ViewModels/RandomCarViewModel.cs
@@ -0,0 +1,14 @@
+﻿using CarRental.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarRental.ViewModels
+{
+    public class RandomCarViewModel
+    {
+        public Car Car { get; set; }
+        public List<Customer> Customers{ get; set; }
+    }
+}
