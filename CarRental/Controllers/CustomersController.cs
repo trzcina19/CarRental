83675de05de3799b2a8a7613cb0714b612893dbd
trzcina19 <trzcinski.michal.mt@gmail.com>
2018-12-13 01:16:08 +0100

    DataTables with Ajax Source (Customer)

--- CarRental/Controllers/CustomersController.cs
+++ CarRental/Controllers/CustomersController.cs
@@ -69,9 +69,9 @@ namespace CarRental.Controllers
 
         public ViewResult Index()
         {
-            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
-
-            return View(customers);
+            //   var customers = _context.Customers.Include(c => c.MembershipType).ToList();
+            //  return View(customers);
+            return View();
         }
 
         //public ActionResult Details(int id)
